3e7ad69ce235002517d04d61b20e96c9cc19af69
Gao Song <samasong@SamadeMacBook-Air.local>
2026-01-22 10:37:42 +0800

    feat:  学习mockjs使用

--- src/common/components/MyInputX.jsx
+++ src/common/components/MyInputX.jsx
@@ -4,11 +4,15 @@ import { ElInput } from "element-plus";
 export default defineComponent(
   (props, ctx) => {
     const vm = getCurrentInstance()
-
     // 定义的 expose 被传给 ElInput 的 ref，相当于一个 ref 回调。Element Plus 的 ElInput 在渲染后会把自身实例传给这个回调，函数内部再把该实例写入vm.exposed。
     // 这样父组件如果对 <MyInputX ref="xxx" /> 取引用，就能直接拿到 ElInput 的公开方法/属性（focus、blur 等），因为 vm.exposed 会成为外部能访问到的对外暴露对象。
     const expose = (v) => {
-      vm.exposed = v
+      vm.exposed = {
+        ...v,
+        handleLog() {
+          console.log('aaa ==> ', 'aaa')
+        }
+      }
     }
 
     // ElInput 支持插槽渲染方法
@@ -30,7 +34,7 @@ export default defineComponent(
             <div>
               { ctx.slots.footer() }
             </div>
-          ) : (<span>footer插槽位置</span>)
+          ) : (<div>footer插槽位置</div>)
         }
       </div>
     )
--- src/components/forTest/index.vue
+++ src/components/forTest/index.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="content-container">
 <!--    <MyInput ref="InputRef" v-model="modelValue"></MyInput>-->
-    <MyInputX v-model="modelValue">
+    <MyInputX v-model="modelValue" ref="myInputXRef">
       <template #append>append</template>
     </MyInputX>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { nextTick, onMounted, ref, useTemplateRef } from "vue";
+import Mock from 'mockjs'
 // 引入组件
 import MyInput from "@/common/components/MyInput.vue";
 import MyInputX from "@/common/components/MyInputX.jsx";
 
 const modelValue = ref('')
+const myInputXRef = useTemplateRef('myInputXRef')
+
+const template = {
+  'list|5-10': [{
+    'id|+1': 1,
+    'name': '@cname',
+    'age|18-60': 1,
+    'email': '@email',
+    'date': '@date'
+  }]
+};
+const data = Mock.mock(template);
+console.log('data ==> ', data)
+
+onMounted(async () => {
+  await nextTick()
+})
 </script>
 
 <style scoped lang="scss">
